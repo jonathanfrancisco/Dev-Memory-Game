b5a8aa2c59d8696f4016e59ba3897bcb7f338830
jonathanfrancisco <jonathanfrancisco999@gmail.com>
2018-01-20 14:27:49 +0800

    Added game logic *MVP*

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ function makePair(baseTiles) {
 
 function getRandomTile(tilePairs) {
     let randomIndice = Math.floor(Math.random()*tilePairs.length);
-    console.log(tilePairs[randomIndice]+' tile will be now removed');
     return tilePairs.splice(randomIndice, 1);
 }
 
@@ -20,20 +19,82 @@ function appendTile(tile) {
         h4.innerHTML = tile;
         div.appendChild(h4);
         div.style.color = '#283593';
+        addListener(div);
     document.querySelector('.board').appendChild(div);
 }
 
+function flipTile(tile) {
+    tile.style.color = '#FFF';
+}
+
+function unflipTile(tile) {
+    tile.style.color = "#283593";
+}
+
+function unflipTiles() {
+    document.querySelectorAll('.board__tile').forEach(function(tile){
+        unflipTile(tile);
+    });
+}
+
+function addListener(tile) {
+
+    tile.addEventListener('click', function() {
+
+        this.style.color = '#FFF';
+        flipTile(this);
+        
+        if(previouslyFlipped !== null) {
+            const previouslyFlippedText = previouslyFlipped.innerHTML;
+            const currentTileText = this.innerHTML;
+            if(previouslyFlippedText === currentTileText) {}
+            else if(previouslyFlippedText !== currentTileText) {
+                window.setTimeout(function() {
+                    unflipTiles();
+                },500);
+            }
+            previouslyFlipped = null;
+            
+        }
+        else 
+            previouslyFlipped = tile;
+    });
+
+}
+
+function initGame(baseTiles) {
+    layTiles(makePair(baseTiles));
+}
+
+const board = document.querySelector('.board');
+let pairFlipCount = 0;
+let previouslyFlipped = null;
+
 let baseTiles = [
     'Hello World',
-    'Ruby',
     'Python',
-    'Ruby On Rails',
-    'Spring',
-    'Java'
+    'Ruby On Rails'
 ];
 
-let pairTiles = makePair(baseTiles);
-layTiles(pairTiles);
+
+initGame(baseTiles);
+
+
+
+
+// document.querySelector('.board__tile').addEventListener('click', (event) => {
+//     alert("WHAT DA HECK?");
+//     alert(event);
+//     // if(event.target.className === 'board__tile' 
+//     //    || event.target.tagName === 'H4') {
+      
+//     //     event.target.style.color = '#FFF';
+//     //     pairFlipCount++;    
+
+//     // }
+
+// });
+
 
 
 // let pairTiles = makePair(textBaseTiles);
